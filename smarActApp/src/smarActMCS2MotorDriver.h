21911beb037dffa3fe32ceb5d4d92f014d379660
Torsten Bögershausen <torsten.bogershausen@esss.se>
2024-06-19 08:08:11 +0200

    Add asyn parameter READBACK
    Add the readback in picometers:
    Store it in a new asynParameter
    A record will be added to this later

--- smarActApp/src/smarActMCS2MotorDriver.h
+++ smarActApp/src/smarActMCS2MotorDriver.h
@@ -82,6 +82,7 @@ const unsigned short   STOP_ON_REF_FOUND       = 0x0020;
 #define MCS2PstatString "PSTAT"
 #define MCS2RefString "REF"
 #define MCS2CalString "CAL"
+#define MCS2ReadbackString "READBACK"
 #define MCS2ErrTxtString "ErrTxt"
 #define MCS2HoldString "HOLD"
 
@@ -138,6 +139,7 @@ protected:
   int pstatrb_; /**< positoner status word readback */
   int ref_;  /**< reference command */
   int cal_;  /**< calibration command */
+  int readback_; /** readback in picometer */
   int errTxt_;
   int hold_; /** hold time */
 #define LAST_MCS2_PARAM hold_
