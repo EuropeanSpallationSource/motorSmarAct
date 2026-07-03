2cff00423ac62242f377b23541966e25b5a27f24
Torsten Bögershausen <torsten.bogershausen@esss.se>
2026-07-03 16:01:29 +0200

    smarActMCS2MotorDriver: Set position from HLSPosition on external LS
    Make use of HLSPosition and LLSPosition:
    When the externanal limit is activated, set position to
    HLSPosition/LLSPosition.

--- smarActApp/src/smarActMCS2MotorDriver.cpp
+++ smarActApp/src/smarActMCS2MotorDriver.cpp
@@ -1003,16 +1003,37 @@ asynStatus MCS2Axis::setIntegerParam(int function, epicsInt32  value) {
   }
   else if (function == pC_->externalLS_) {
     /* an external limit switch may have been hit */
-    int oldDone = 0;
-    int oldExternalLS = 0;
-    (void)pC_->getIntegerParam(axisNo_, pC_->motorStatusDone_,  &oldDone);
-    (void)pC_->getIntegerParam(axisNo_, pC_->externalLS_,  &oldExternalLS);
-    int newActivedLS = (oldExternalLS ^ value) & value;
+    int actDone = 0;
+    int actExternalLS = 0;
+    (void)pC_->getIntegerParam(axisNo_, pC_->motorStatusDone_,  &actDone);
+    (void)pC_->getIntegerParam(axisNo_, pC_->externalLS_,  &actExternalLS);
+    int newActivedLS = (actExternalLS ^ value) & value;
     asynPrint(pC_->pasynUserController_, ASYN_TRACE_INFO,
-              "%s(%d) oldDone=%d oldExternalLS_=%d value=%d newActivedLS=%d\n",
-              functionName, axisNo_, oldDone, oldExternalLS, value, newActivedLS);
-    if (!oldDone && newActivedLS) {
-        stop(0.0);
+              "%s(%d) actDone=%d actExternalLS_=%d value=%d newActivedLS=%d\n",
+              functionName, axisNo_, actDone, actExternalLS, value, newActivedLS);
+    if (!actDone && newActivedLS) {
+      if(sensorPresent_ && !sensorIsDisabled_) {
+        ; /* don't update the position if we have and use a sensor */
+      } else if (newActivedLS == 2) {
+        /* We are at the hLSPosition (or close to it) */
+        double hLSPosition;
+        if  (asynSuccess == pC_->getDoubleParam(axisNo_,
+                                                pC_->HLSPosition_,
+                                                &hLSPosition)) {
+          asynMotorAxis::setDoubleParam(pC_->motorPosition_,
+                                        hLSPosition / PULSES_PER_STEP);
+        }
+      } else if (newActivedLS == 1) {
+        /* We are at the lLSPosition (or close to it) */
+        double lLSPosition;
+        if  (asynSuccess == pC_->getDoubleParam(axisNo_,
+                                                pC_->LLSPosition_,
+                                                &lLSPosition)) {
+          asynMotorAxis::setDoubleParam(pC_->motorPosition_,
+                                        lLSPosition / PULSES_PER_STEP);
+        }
+      }
+      stop(0.0);
     }
   }
   else if (function == pC_->sensorPowerMode_) {
